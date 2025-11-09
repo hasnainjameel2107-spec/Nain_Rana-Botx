1a276480ff2d28fd3ba6d06511dadc313d0c7a3a
hasnainjameel2107-spec <hasnainjameel2107@gmail.com>
2025-11-09 12:52:09 +0400

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -40,7 +40,7 @@ cmd({
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
                 newsletterJid: '120363348739987203@newsletter',
-                newsletterName: config.OWNER_NAME,
+                newsletterName: config.Owner_Name,
                 serverMessageId: 143
             }
         };
